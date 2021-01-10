d760d5b9df21f41ed6d30b4c0b18426fc48ef997
Mrau Hu <mrauhu@yandex.ru>
2021-01-10 07:40:46 +0300

    Created rules for Polish cyrillization based on book from 1865.

---
+++ src/lang/pl/cyrillic1865.ts
@@ -0,0 +1,55 @@
+import { Rules } from '../rules';
+
+// noinspection NonAsciiCharacters
+export const POLISH_MODERN_ALPHABET_TO_CYRILLIC_1865: Rules = {
+  a: 'а',
+  ą: 'ą',
+  b: 'б',
+  c: 'ц',
+  ć: 'ч',
+  d: 'д',
+  e: 'э',
+  ę: 'ę',
+  f: 'ф',
+  g: 'г',
+  h: 'x̾',
+  i: 'и',
+  j: 'й',
+  k: 'к',
+  l: 'л',
+  ł: 'л', // new
+  m: 'м',
+  n: 'н',
+  ń: 'нь', // new
+  o: 'о',
+  ó: 'ô',
+  p: 'п',
+  r: 'р',
+  s: 'с',
+  ś: 'ш',
+  t: 'т',
+  u: 'у',
+  w: 'в',
+  y: 'ы',
+  z: 'з',
+  ż: 'ж',
+  ź: 'зь', // new
+};
+// noinspection JSNonASCIINames,NonAsciiCharacters
+export const POLISH_OLD_DIGRAPHS_TO_CYRILLIC_1865: Rules = {
+  ie: 'е',
+  io: 'ё',
+  iu: 'ю',
+  ió: 'ю̂',
+  ia: 'я',
+  ią: 'я̨',
+  ch: 'х',
+  rz: 'p̌',
+  cz: 'ч',
+  sz: 'ш',
+  szcz: 'щ',
+};
+// noinspection NonAsciiCharacters
+export const POLISH_MODERN_DIGRAPHS_TO_CYRILLIC_1865: Rules = {
+  ść: 'щ',
+};
