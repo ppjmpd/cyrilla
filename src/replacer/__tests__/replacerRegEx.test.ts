4249e934378a54929f58177a87cfcb9eadff2ea2
Mrau Hu <mrauhu@yandex.ru>
2021-01-10 13:01:06 +0300

    Renamed `transliterator` to `replacer` in `replacerRegEx.test.ts`

--- src/replacer/__tests__/replacerRegEx.test.ts
+++ src/replacer/__tests__/replacerRegEx.test.ts
@@ -1,14 +1,14 @@
 import { ReplacerRegEx } from '../replacerRegEx';
 
 describe('ReplacerRegEx class', () => {
-  let transliterator: ReplacerRegEx;
+  let replacer: ReplacerRegEx;
   const rules = {
     '^a': 'а',
     b: 'б',
     c$: 'ц',
   };
   beforeAll(() => {
-    transliterator = new ReplacerRegEx(rules);
+    replacer = new ReplacerRegEx(rules);
   });
 
   describe('replace()', () => {
@@ -16,16 +16,16 @@ describe('ReplacerRegEx class', () => {
     const result = Object.values(rules).join('');
 
     it('should replace characters in string', () => {
-      expect(transliterator.replace(word)).toEqual(result);
+      expect(replacer.replace(word)).toEqual(result);
     });
 
     it('should replace uppercase characters in string', () => {
-      expect(transliterator.replace(word.toUpperCase(), [0, 1, 2])).toEqual(result.toUpperCase());
+      expect(replacer.replace(word.toUpperCase(), [0, 1, 2])).toEqual(result.toUpperCase());
     });
 
     it('should return original string if none matched', () => {
       const word = 'def';
-      expect(transliterator.replace(word)).toEqual(word);
+      expect(replacer.replace(word)).toEqual(word);
     });
   });
 });
